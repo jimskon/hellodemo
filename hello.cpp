19f3128e8ffb3cfbef930ed1697d63d7cc0e10a1
jimskon <jamesskon@gmail.com>
2023-01-31 19:18:23 +0000

    Added sunny!

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
 	cout << "Hello Kenyon SCMP318!!!" << endl;
-	cout << "What a wonderful day!!" << endl;
+	cout << "What a wonderful sunny day!!" << endl;
 	
 	for (int i=0; i< 10; i++) {
 		cout << "git";
