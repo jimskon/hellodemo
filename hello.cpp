8494858d3d8c8a1ac5d700d9704b76fbcd79e0f0
jimskon <jamesskon@gmail.com>
2023-01-31 14:00:11 -0500

    Add an extra message

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
 
-	cout <<" Hello Kenyon SCMP318!!!" << endl;
+	cout << "Hello Kenyon SCMP318!!!" << endl;
+	cout << "What a wonderful day!!" << endl;
 	exit(0);
 }
